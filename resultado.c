f58ebbb584ebf691ebd90b2452e7e9e551b70df3
guitolin <g35844579@gmail.com>
2026-09-09 17:22:50 -0300

    adicionei a função estado_instancia que classifica as instancias

--- resultado.c
+++ resultado.c
@@ -4,3 +4,15 @@ void registrar_execucao(RegistroExecucao *registros, int tempo, int instancia){
     registros[tempo].executou = 1;
     registros[tempo].instancia = instancia;
 }
+
+char estado_instancia(Instancia *instancia){
+    if (instancia->concluida){
+        return 'F';
+    }
+
+    if (instancia->perdida){
+        return 'L';
+    }
+
+    return 'H';
+}
--- resultado.h
+++ resultado.h
@@ -1,11 +1,14 @@
 #ifndef RESULTADO_H
 #define RESULTADO_H
 
+#include "tarefa.h"
+
 typedef struct RegistroExecucao{
     int executou;
     int instancia;
 } RegistroExecucao;
 
 void registrar_execucao(RegistroExecucao *registros, int tempo, int instancia);
+char estado_instancia(Instancia *instancia);
 
 #endif
